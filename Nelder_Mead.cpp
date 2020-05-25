3cf7ed99419145c19c980d5337dd35c01c4fa720
spitefulowl <54745281+spitefulowl@users.noreply.github.com>
2020-05-25 23:50:06 +0300

    Limitations (#1)
    * Problem solver with limitations
    
    * Problem solver with limitations (2)
    
    * Test another function
    
    * Improvements
    
    * Improvements (2)

--- Nelder_Mead.cpp
+++ Nelder_Mead.cpp
@@ -3,7 +3,7 @@
 #include "pch.h"
 #include <iostream>
 #include "utils.h"
-#include "unconditional.h"
+#include "nelder_mead.h"
 
 template<typename Point>
 struct first {
@@ -28,45 +28,60 @@ struct third {
 		return std::pow(value[0] + 10 * value[1], 2) + 5 * std::pow(value[2] - value[3], 2) + std::pow(value[1] - 2 * value[2], 4) + 10 * std::pow(value[0] - value[3], 4);
 	}
 };
+#define ACCURACY 0.000001
 
 int main()
 {
 	{
+		std::vector<std::pair<double, double>> allowed_values = {
+														{ -1.2, 0 },
+														{ 1, 3 }
+		};
 		point<double, 2> base(-1.2, 1.);
 		first<point<double, 2>> function{};
-		//unconditional<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 0.28, base, function);
-		//auto result = cond.get_solution(0.000001);
-		unconditional<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 5.3, base, function);
-		auto result = cond.get_solution(0.000001);
+		//nelder_mead<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 0.28, base, function);
+		//auto result = cond.get_solution(ACCURACY);
+		nelder_mead<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 1.3, base, function);
+		auto result = cond.get_solution(ACCURACY, allowed_values);
 		for (auto& elem : result) {
 			printf("Point: %f %f\n", elem[0], elem[1]);
-			printf("Criterion: %.8f\n", function(elem));
+			printf("Criterion: %.12f\n", function(elem));
 		}
 	}
 	printf("\n\n\n");
 	{
+		std::vector<std::pair<double, double>> allowed_values = {
+														{ -1.2, -0.5 },
+														{ -1, 0 }
+		};
 		point<double, 2> base(-1.2, -1.);
 		second<point<double, 2>> function{};
-		//unconditional<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 0.28, base, function);
-		//auto result = cond.get_solution(0.000001);
-		unconditional<double, second<point<double, 2>>, 2> cond(1., 2., 0.5, 5, base, function);
-		auto result = cond.get_solution(0.000001);
+		//nelder_mead<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 0.28, base, function);
+		//auto result = cond.get_solution(ACCURACY);
+		nelder_mead<double, second<point<double, 2>>, 2> cond(1., 2., 0.5, 1, base, function);
+		auto result = cond.get_solution(ACCURACY, allowed_values);
 		for (auto& elem : result) {
 			printf("Point: %f %f\n", elem[0], elem[1]);
-			printf("Criterion: %.8f\n", function(elem));
+			printf("Criterion: %.12f\n", function(elem));
 		}
 	}
 	printf("\n\n\n");
 	{
+		std::vector<std::pair<double, double>> allowed_values = {
+														{ 3, 4 },
+														{ -2, -1 },
+														{ -3, 0 },
+														{ 1, 4 }
+		};
 		point<double, 4> base(3., -1., 0., 1.);
 		third<point<double, 4>> function{};
-		//unconditional<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 0.28, base, function);
-		//auto result = cond.get_solution(0.000001);
-		unconditional<double, third<point<double, 4>>, 4> cond(1., 2., 0.5, 5, base, function);
-		auto result = cond.get_solution(0.0000001);
+		//nelder_mead<double, first<point<double, 2>>, 2> cond(1., 2., 0.5, 0.28, base, function);
+		//auto result = cond.get_solution(ACCURACY);
+		nelder_mead<double, third<point<double, 4>>, 4> cond(1., 2., 0.5, 2.5, base, function);
+		auto result = cond.get_solution(ACCURACY, allowed_values);
 		for (auto& elem : result) {
 			printf("Point: %f %f %f %f\n", elem[0], elem[1], elem[2], elem[3]);
-			printf("Criterion: %.8f\n", function(elem));
+			printf("Criterion: %.12f\n", function(elem));
 		}
 	}
 }
